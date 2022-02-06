6b4a41a1960dd1ce41a3b8e242fe4a648802c237
sumaiya ahmed <sumu0612@gmail.com>
2022-02-07 04:14:15 +0600

    1st commit

---
+++ js/transaction.js
@@ -0,0 +1,55 @@
+let depositeArea = document.getElementById('deposit-area');
+let withdrawArea = document.getElementById('withdraw-area');
+
+let depositBtn = document.getElementById('deposit-btn');
+let withdrawBtn = document.getElementById('withdraw-btn');
+
+
+
+
+//event listener for deposite
+depositBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    let inputDeposite = document.getElementById('deposit-amt');
+    let depositCount = document.getElementById('curr-deposit');
+    let balanceCount = document.getElementById('total-balance').innerText;
+    let depoNewAmount = inputDeposite.value;
+    let depoPreAmount = depositCount.innerText;
+    let totalDepo = parseInt(depoNewAmount) + parseInt(depoPreAmount);
+    depositCount.innerHTML = totalDepo;
+    inputDeposite.value = '';
+    let totalBalance = parseInt(balanceCount) + parseInt(depoNewAmount);
+    
+    document.getElementById('total-balance').innerText = totalBalance;
+    // console.log('deposite btn click');
+    // console.log(depositCount.innerHTML);
+    // console.log(balanceCount);
+})
+
+withdrawBtn.addEventListener('click',function(e){
+    e.preventDefault();
+    let inputWithdraw = document.getElementById('withdraw-amt');
+    let withdrawCount = document.getElementById('curr-withdraw').innerText;
+
+    let withdrawAmount = inputWithdraw.value;
+    let balanceCount = document.getElementById('total-balance').innerText;
+    let balaceDeduct = parseInt(balanceCount) - parseInt(withdrawAmount);
+
+    if(balaceDeduct > 0){
+        document.getElementById('total-balance').innerText = balaceDeduct;
+        let addWithdraw = parseInt(withdrawCount) + parseInt(withdrawAmount);
+        document.getElementById('curr-withdraw').innerText = addWithdraw;
+    }
+    else{
+        alert("Insufficient Balance!!!!");
+    }
+   
+
+    inputWithdraw.value = '';
+
+    //add the deducted amount into withdrawn
+   
+    
+
+})
+
